11e86c9fd33cb5c5f0ec2576ed674fa0bc36850b
samina anjum <159903174+samina-anjum@users.noreply.github.com>
2024-04-29 23:26:05 +0500

    Add files via upload

---
+++ 3. Name Cases/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+//QUETION 2# Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "SAMINA ANJUM IMRAN";
+let message = "";
+console.log(personName.toLowerCase(), personName.toUpperCase(), personName.charAt(0).toUpperCase() + personName.slice(1));
---
+++ 3. Name Cases/main.ts
@@ -0,0 +1,8 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+//QUETION 2# Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName :string ="SAMINA ANJUM IMRAN";
+let message :string = ""
+console.log(personName.toLowerCase(), personName.toUpperCase(), personName.charAt(0).toUpperCase() + personName.slice(1));
+
